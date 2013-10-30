532033c972b90787b804b980b054a7da84233833
Thomas Brisbout <thomas.brisbout@gmail.com>
2013-10-31 00:36:08 +0100

    Validates email format and uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131030195055) do
+ActiveRecord::Schema.define(version: 20131030232534) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -19,5 +19,7 @@ ActiveRecord::Schema.define(version: 20131030195055) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
